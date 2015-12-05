b68ef3d9d64c391e598ee910d3f502d450c97663
Thomas Rientjes <synzvato@protonmail.com>
2015-12-05 01:17:56 +0100

    Update preference system

---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,4 @@
+pref('extensions.id1-BoFifL9Vbdl2zQ@jetpack.addNotice', true);
+pref('extensions.id1-BoFifL9Vbdl2zQ@jetpack.blockMissing', false);
+pref('extensions.id1-BoFifL9Vbdl2zQ@jetpack.domainWhitelist', '');
+pref('extensions.id1-BoFifL9Vbdl2zQ@jetpack.amountInjected', 0);
