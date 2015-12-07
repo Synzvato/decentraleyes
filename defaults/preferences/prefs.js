e21287ec0b974d51e0e1b11bcaa905d34154b42e
Thomas Rientjes <synzvato@protonmail.com>
2015-12-07 03:09:19 +0100

    Update preference identifiers

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,4 +1,4 @@
-pref('extensions.id1-BoFifL9Vbdl2zQ@jetpack.addNotice', true);
-pref('extensions.id1-BoFifL9Vbdl2zQ@jetpack.blockMissing', false);
-pref('extensions.id1-BoFifL9Vbdl2zQ@jetpack.domainWhitelist', '');
-pref('extensions.id1-BoFifL9Vbdl2zQ@jetpack.amountInjected', 0);
+pref('extensions.jid1-BoFifL9Vbdl2zQ@jetpack.addNotice', true);
+pref('extensions.jid1-BoFifL9Vbdl2zQ@jetpack.blockMissing', false);
+pref('extensions.jid1-BoFifL9Vbdl2zQ@jetpack.domainWhitelist', '');
+pref('extensions.jid1-BoFifL9Vbdl2zQ@jetpack.amountInjected', 0);
